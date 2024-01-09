31ee9e1385fcd0e2bc996d6b06051e25e5f47cd3
Jonathan Hope <jhope@fastmail.com>
2024-01-08 20:03:28 -0800

    Brew formula update for armaria version v0.0.24

--- Formula/armaria.rb
+++ Formula/armaria.rb
@@ -5,13 +5,13 @@
 class Armaria < Formula
   desc "Armaria is a fast, open, and local first bookmarks manager."
   homepage "https://github.com/JonathanHope/armaria"
-  version "0.0.23"
+  version "0.0.24"
   license "MIT"
   depends_on :macos
 
   on_macos do
-    url "https://github.com/JonathanHope/armaria/releases/download/v0.0.23/armaria_0.0.23_darwin_all.zip", using: CurlDownloadStrategy
-    sha256 "c0fe5c671e931a997c4154fa89a5771892af5edacb0ab0da782f560c280d0068"
+    url "https://github.com/JonathanHope/armaria/releases/download/v0.0.24/armaria_0.0.24_darwin_all.zip", using: CurlDownloadStrategy
+    sha256 "677aa95664f453711288390ab7fed4b6c0520d655135f39c0a3282cea4025e86"
 
     def install
       bin.install "armaria"
